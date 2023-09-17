743c99a2417721881d535e036a9b36e4aad70228
Aayush Joshi <76811479+Aayush-Joshi-01@users.noreply.github.com>
2023-09-17 15:59:57 +0530

    Update Morse Code.py

--- Morse Code/Morse Code.py
+++ Morse Code/Morse Code.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
 
-"""
-Python program to translate to and from Morse code.
-
-https://en.wikipedia.org/wiki/Morse_code
-"""
-
-# fmt: off
 MORSE_CODE_DICT = {
     "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.",
     "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..", "M": "--", "N": "-.",
@@ -17,35 +9,22 @@ MORSE_CODE_DICT = {
     ":": "---...", ",": "--..--", ".": ".-.-.-", "'": ".----.", '"': ".-..-.",
     "?": "..--..", "/": "-..-.", "=": "-...-", "+": ".-.-.", "-": "-....-",
     "(": "-.--.", ")": "-.--.-", "!": "-.-.--", " ": "/"
-}  # Exclamation mark is not in ITU-R recommendation
-# fmt: on
+} 
 REVERSE_DICT = {value: key for key, value in MORSE_CODE_DICT.items()}
 
 
 def encrypt(message: str) -> str:
-    """
-    >>> encrypt("Sos!")
-    '... --- ... -.-.--'
-    >>> encrypt("SOS!") == encrypt("sos!")
-    True
-    """
+    
     return " ".join(MORSE_CODE_DICT[char] for char in message.upper())
 
 
 def decrypt(message: str) -> str:
-    """
-    >>> decrypt('... --- ... -.-.--')
-    'SOS!'
-    """
+  
     return "".join(REVERSE_DICT[char] for char in message.split())
 
 
 def main() -> None:
-    """
-    >>> s = "".join(MORSE_CODE_DICT)
-    >>> decrypt(encrypt(s)) == s
-    True
-    """
+   
     message = "Morse code here!"
     print(message)
     message = encrypt(message)
